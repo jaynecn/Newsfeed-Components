9dd61eff0af20eb4c4c41f728e0230bf08d92674
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-22 14:56:00 +0100

    working on text for expand button

--- Article/Article.js
+++ Article/Article.js
@@ -3,19 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = "expand";
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     
   }
 
   expandArticle() {
+    this.domElement.classList.toggle('article-open');
     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
   }
+
 }
 
 /* START HERE: 
@@ -25,5 +29,6 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
+  
 
-let articles;
+const articles = document.querySelectorAll('.article').forEach(info => new Article(info));
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,12 @@
 
 const toggleMenu = () => {
+  this.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelectorAll('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener('click', toggleMenu);
